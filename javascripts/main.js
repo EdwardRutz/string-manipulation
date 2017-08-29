6d17cba1dd6e5cf31a7d17bfe589dcad328f01b6
Edward R <edwardrz@users.noreply.github.com>
2017-08-29 09:59:10 -0500

    completed main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,22 +2,69 @@
 console.log("main.js active");
 
 /***********  SCRIPT  ***************/
-function reversal() {
+let testString = "";
+let getLetters = document.getElementById("lettersButton");
+let stringOutR = document.getElementById("domOut");
+let stringOutA = document.getElementById("domAlphabetical");
+let stringOutP = document.getElementById("domPalindrome");
+let getEnter = document.getElementById("wordsIn");
 
+
+getLetters.addEventListener("click", changeString);
+
+// Input via Enter Key
+getEnter.addEventListener("keypress", enterKey);
+
+function enterKey(event){
+  if(event.keyCode == 13) {
+    event.preventDefault();
+    changeString();
+  }
 }
 
-function alphabits() {
 
+  // console.log("matches", matches);
+  
+
+
+function changeString() {
+  event.preventDefault();
+
+ testString = document.getElementById("wordsIn").value;
+  
+ var matches = testString.match(/\d+/g);
+  console.log("matches", matches);
+  if (matches !== null) {
+      alert('Enter only letters; no numbers allowed');
+      } else {
+
+          let reverse = reversal(testString);
+          let alphabetical = alphabits(testString);
+          let palindrome = testPalindrome(testString);
+
+          stringOutR.innerHTML = `Reversed String is:  ${reverse}`;
+          stringOutA.innerHTML = `Alphabetical Order is:  ${alphabetical}`;
+        } 
+}
+    
+//Functions
+function reversal(testString) {
+  return testString.split('').reverse().join('');
 }
 
-function palindrome() {
-
+function alphabits(letters) {
+  return letters.split('').sort().join('');
 }
 
-var testString = "";
-reversal(testString);
-alphabits(testString);
-palindrome(testString);
+function testPalindrome(letters) {
+  let reverseString = letters.split("").reverse().join("");
+  if (reverseString == letters){
+    return stringOutP.innerHTML = `The string ${testString} is a palindrome.`;
+  } else {
+      return stringOutP.innerHTML = `The string ${testString} is not a palindrome`;
+    }
+} 
+
 
 
 
